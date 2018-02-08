8be5d6deb32ecca4232ef2b2178fb78bff0f0576
Avi Aryan <avi.aryan123@gmail.com>
2018-02-08 13:47:36 +0530

    implement basic socketio

--- server.js
+++ server.js
@@ -5,33 +5,19 @@
 var express = require('express');
 var app = express();
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.send('Hi. This is the root');
 });
-
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
