b6f70792baf1d4315e5c219a98bb62585b0b9a24
Avi Aryan <avi.aryan123@gmail.com>
2018-02-09 19:04:32 +0530

    basic chat implemented

--- server.js
+++ server.js
@@ -1,18 +1,43 @@
-// server.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// global variables
+let channels = {};
+
+// magic
 io.on('connection', function (socket) {
   console.log('a user connected');
+  let userStatus = {};
+
   socket.on('disconnect', function () {
     console.log('user disconnected');
   });
+
+  // join channel
+  socket.on('/join', (msg) => {
+    console.log(msg);
+    let ch = msg.channel;
+
+    if (channels.hasOwnProperty(ch)){
+      // channel exists
+      channels[ch]['population']++;
+    } else {
+      // need to create channel
+      channels[ch] = {population: 1};
+    }
+
+    // broadcast status message
+    io.emit('/msg ' + ch, {user: null, message: `${msg.user} joined! Total members = ${channels[ch]['population']}`});
+
+    // setup message listener
+    socket.on('/msg ' + ch, (msg) => {
+      io.emit('/msg ' + ch, msg);
+    });
+  });
+
 });
 
 app.get("/", function (request, response) {
